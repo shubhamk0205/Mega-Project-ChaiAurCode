f2e44b7e67108e2cfc1408d32fe3a51b3b43092a
Shubham_kapoor <kapoorshubham2005@gmail.com>
2025-03-24 14:19:03 +0530

    add user registration route and update request size limits

--- src/app.js
+++ src/app.js
@@ -7,8 +7,15 @@ app.use(cors({
     origin: "process.env.CORS_ORIGIN",
     credentials: true
 }));
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true ,limit: "5mb"}));
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true ,limit: "20kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//router declaration
+app.use("/api/v1/users" , userRouter)
+
 export default app;
