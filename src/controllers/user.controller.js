dcfa9b0e9a295ffa8097cae8b2cbaee3663fcdf2
Shubham_kapoor <kapoorshubham2005@gmail.com>
2025-03-24 19:58:40 +0530

    feat: enhance user registration with validation, image upload, and improved response structure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,69 @@
 import asyncHandler from "../utils/asyncHandler.js"
+import ApiError from "../utils/ApiError.js"
+import User from "../models/User.js"
+import uploadOnCloudinary from "../utils/cloudinary.js"
+import ApiResponse from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { userId, location } = req.body;
-    res.status(200).json({ success: true, message: "Registration successful", data: { userId, location } });
+    //get user details from frontend ---DONE
+    //validation - not empty ---DONE
+    //check if user is alreday registered or not by email and username as they both are unique ----DONE
+    //check for the images . check for avatar----DONE
+    // upload them to cloudnary , check avatar ---DONE
+    // create user object - create entry in Db ---DONE
+    //remove password and refresh token field from the response ---DONE
+    //check for the user creation ---DONE
+    //return response ---DONE
+
+    const { username, email, fullname, password } = req.body;
+    console.log( email, fullname, password);
+
+    if(fullname==="" || email==="" || password==="" || username===""){
+        throw new ApiError (400, "All fields are required")
+    }
+
+    const existedUser =await User.findOne({
+        $or:[{   email  },{   username  }]
+    });
+    if(existedUser){
+        throw new ApiError(409, "User already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(500, "Error uploading images")
+    }
+
+    const user = await User.create({
+        username : username.toLowerCase(),
+        email,
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password
+    });
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    if(!createdUser){
+        throw new ApiError(500, "Error creating user")
+    }
+
+    return res.status(201).json(new ApiResponse(201, createdUser ,"User created successfully!!"));
 });
 
-export { registerUser }
+export {
+    registerUser,
+
+}
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,9 +1,10 @@
 class ApiResponse {
-  constructor(statusCode , message="sucess", data) {
+  constructor(statusCode, message = "success", data) {
     this.statusCode = statusCode;
     this.success = statusCode >= 200 && statusCode < 300;
-    this.success = success;
     this.message = message;
     this.data = data;
   }
 }
+
+export default ApiResponse;
